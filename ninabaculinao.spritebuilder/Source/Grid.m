deb4ad60511da9d6f5b94dc3eaf6ae1c00d199fe
Nina Baculinao <nb2406@columbia.edu>
2014-07-21 21:06:42 -0400

    Added swipe gestures, behaving oddly

--- ninabaculinao.spritebuilder/Source/Grid.m
+++ ninabaculinao.spritebuilder/Source/Grid.m
@@ -18,12 +18,11 @@
     
     NSMutableArray *_gridArray; // a 2d array
     NSNull *_noTile;
-
 }
 
 // two constants to describe the amount of rows and columns
-static const int GRID_ROWS = 12;
-static const int GRID_COLUMNS = 6;
+static const NSInteger GRID_ROWS = 12;
+static const NSInteger GRID_COLUMNS = 6;
 
 - (void)didLoadFromCCB{
     
@@ -41,6 +40,30 @@ static const int GRID_COLUMNS = 6;
 	}
     
     [self spawnDice];
+    
+//    // listen for swipes to the left
+//    UISwipeGestureRecognizer * swipeLeft= [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeLeft)];
+//    swipeLeft.direction = UISwipeGestureRecognizerDirectionLeft;
+//    [[[CCDirector sharedDirector] view] addGestureRecognizer:swipeLeft];
+//    // listen for swipes to the right
+//    UISwipeGestureRecognizer * swipeRight= [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeRight)];
+//    swipeRight.direction = UISwipeGestureRecognizerDirectionRight;
+//    [[[CCDirector sharedDirector] view] addGestureRecognizer:swipeRight];
+//    // listen for swipes down
+//    UISwipeGestureRecognizer * swipeDown= [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeDown)];
+//    swipeDown.direction = UISwipeGestureRecognizerDirectionDown;
+//    [[[CCDirector sharedDirector] view] addGestureRecognizer:swipeDown];
+//    // listen for swipes up
+//    UISwipeGestureRecognizer * swipeUp= [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeUp)];
+//    swipeUp.direction = UISwipeGestureRecognizerDirectionUp;
+//    [[[CCDirector sharedDirector] view] addGestureRecognizer:swipeUp];
+	
+}
+
+- (void) update:(CCTime) delta {
+    //    _dice.position = ccp(_dice.position.x, _dice.position.y - delta*100.0);
+    //    _seconddice.position = ccp(_seconddice.position.x, _seconddice.position.y - delta*100.0);
+    //}
 }
 
 # pragma mark - Create grid
@@ -70,44 +93,13 @@ static const int GRID_COLUMNS = 6;
         
 		for (int j = 0; j < GRID_COLUMNS; j++) {
 			//  iterate through each column in the current row
-
-//            Tile *tile = [[Tile alloc] initTile];
-//            tile.position = ccp(x, y);
-//            [self addChild:tile]; // problem here without initialization
-//            // this is shorthand to access an array inside an array
-//            _gridArray[i][j] = tile;
-//            tile.isOccupied = TRUE; // debugging to see placement
-            
-//            CCNodeColor *backgroundTile = [CCNodeColor nodeWithColor:[CCColor clearColor]];
-//			backgroundTile.contentSize = CGSizeMake(_tileWidth, _tileHeight);
-//			backgroundTile.position = ccp(x, y);
-//			[self addChild:backgroundTile];
-            
 			x+= _tileWidth + _tileMarginHorizontal; // after positioning a block increase x variable
 		}
 		y+= _tileHeight + _tileMarginVertical; // after completing row increase y variable
 	}
 }
 
-# pragma mark - spawn random tiles
-
-- (CGPoint)positionForTile:(NSInteger)column row:(NSInteger)row {
-	NSInteger x = _tileMarginHorizontal + column * (_tileMarginHorizontal + _tileWidth);
-	NSInteger y = _tileMarginVertical + row * (_tileMarginVertical + _tileHeight);
-	return CGPointMake(x,y);
-}
-
-- (void)addDieAtTile:(NSInteger)column row:(NSInteger)row {
-    Dice* die = [self randomizeNumbers];
-	_gridArray[row][column] = die;
-	die.scale = 0.f;
-	[self addChild:die];
-	die.position = [self positionForTile:column row:row];
-	CCActionDelay *delay = [CCActionDelay actionWithDuration:0.3f];
-	CCActionScaleTo *scaleUp = [CCActionScaleTo actionWithDuration:0.2f scale:1.f];
-	CCActionSequence *sequence = [CCActionSequence actionWithArray:@[delay, scaleUp]];
-	[die runAction:sequence];
-}
+# pragma mark - Create random Dice
 
 -(Dice*) randomizeNumbers {
     int random = arc4random_uniform(5)+1;
@@ -146,31 +138,177 @@ static const int GRID_COLUMNS = 6;
     return die;
 }
 
+- (CGPoint)positionForTile:(NSInteger)column row:(NSInteger)row {
+	NSInteger x = _tileMarginHorizontal + column * (_tileMarginHorizontal + _tileWidth);
+	NSInteger y = _tileMarginVertical + row * (_tileMarginVertical + _tileHeight);
+	return CGPointMake(x,y);
+}
+
+- (void)addDieAtTile:(NSInteger)column row:(NSInteger)row {
+    Dice* die = [self randomizeNumbers];
+	_gridArray[row][column] = die;
+	die.scale = 0.f;
+	[self addChild:die];
+	die.position = [self positionForTile:column row:row];
+	CCActionDelay *delay = [CCActionDelay actionWithDuration:0.3f];
+	CCActionScaleTo *scaleUp = [CCActionScaleTo actionWithDuration:0.2f scale:1.f];
+	CCActionSequence *sequence = [CCActionSequence actionWithArray:@[delay, scaleUp]];
+	[die runAction:sequence];
+ 
+//    CCActionMoveTo *fall = [CCActionMoveTo actionWithDuration:5.0f position:ccp(die.position.x, 0)];
+//    CCActionMoveTo *fallSequence = [CCActionSequence actionWithArray:@[delay, fall]];
+//    [die runAction:fallSequence];
+}
+
+//
+//- (void)fallingDie {
+//    BOOL falling = FALSE;
+//    while (!falling) {
+//        _gridArray[newX][newY] = _gridArray[oldX][oldY];
+//        _gridArray[oldX][oldY] = _noTile;
+//    }
+//}
+
 - (void)spawnDice {
 	BOOL spawned = FALSE;
 	while (!spawned) {
-		NSInteger randomRow = arc4random_uniform(2)+10; // int bt 11 and 12
-		NSInteger randomColumn = arc4random_uniform(5);
-        CCLOG(@"Column %ld, Row %ld", (long)randomColumn, (long)randomRow);
-        NSInteger nextRow = arc4random_uniform(2)+randomRow;
+		NSInteger firstRow = 11;
+		NSInteger firstColumn = arc4random_uniform(5); // int bt 0 and 4
+        CCLOG(@"First Column %ld, Row %ld", (long)firstColumn, (long)firstRow);
+        NSInteger nextRow = firstRow - arc4random_uniform(2);
         NSInteger nextColumn;
-        if (nextRow != randomRow) {
-            nextColumn = randomColumn;
+        if (firstRow != nextRow) {
+            nextColumn = firstColumn;
         } else {
-            nextColumn = randomColumn+1;
+            nextColumn = firstColumn+1;
         }
-        CCLOG(@"Next column %ld, next row %ld", (long)nextColumn, (long)nextRow);
+        CCLOG(@"Next Column %ld, Row %ld", (long)nextColumn, (long)nextRow);
         
-        BOOL positionFree = (_gridArray[randomRow][randomColumn] == _noTile);
+        BOOL positionFree = (_gridArray[firstRow][firstColumn] == _noTile);
         BOOL nextPositionFree = (_gridArray[nextRow][nextColumn] == _noTile);
 		if (positionFree && nextPositionFree) {
-			[self addDieAtTile:randomColumn row:randomRow];
+			[self addDieAtTile:firstColumn row:firstRow];
             [self addDieAtTile:nextColumn row:nextRow];
             spawned = TRUE;
         } else {
             CCLOG(@"Game Over");
+            break;
         }
 	}
+}
+
+# pragma mark - Swipe recognition
+
+- (void)swipeLeft {
+    [self move:ccp(-1, 0)];
+}
+
+- (void)swipeRight {
+    [self move:ccp(1, 0)];
+}
+
+- (void)swipeDown {
+    [self move:ccp(0, -1)];
+}
+
+- (void)swipeUp {
+    [self move:ccp(1,0)];
+}
+
+# pragma mark - Move dice
+
+- (void)move:(CGPoint)direction {
+    // apply negative vector until reaching boundary, this way we get the tile that is the furthest away
+    // bottom left corner
+    NSInteger currentX = 0;
+    NSInteger currentY = 0;
+    // Move to relevant edge by applying direction until reaching border
+    while ([self indexValid:currentX y:currentY]) {
+        CGFloat newX = currentX + direction.x;
+        CGFloat newY = currentY + direction.y;
+        if ([self indexValid:newX y:newY]) {
+            currentX = newX;
+            currentY = newY;
+        } else {
+            break;
+        }
+    }
+    // store initial row value to reset after completing each column
+    NSInteger initialY = currentY;
+    // define changing of x and y value (moving left, up, down or right?)
+    NSInteger xChange = -direction.x;
+    NSInteger yChange = -direction.y;
+    if (xChange == 0) {
+        xChange = 1;
+    }
+    if (yChange == 0) {
+        yChange = 1;
+    }
+    // visit column for column
+    while ([self indexValid:currentX y:currentY]) {
+        while ([self indexValid:currentX y:currentY]) {
+            // get tile at current index
+            Dice *die = _gridArray[currentX][currentY];
+            if ([die isEqual:_noTile]) {
+                // if there is no tile at this index -> skip
+                currentY += yChange;
+                continue;
+            }
+            // store index in temp variables to change them and store new location of this tile
+            NSInteger newX = currentX;
+            NSInteger newY = currentY;
+            /* find the farthest position by iterating in direction of the vector until we reach border of grid or an occupied cell*/
+            while ([self indexValidAndUnoccupied:newX+direction.x y:newY+direction.y]) {
+                newX += direction.x;
+                newY += direction.y;
+            }
+            if (newX != currentX || newY !=currentY) {
+                [self moveDice:die fromIndex:currentX oldY:currentY newX:newX newY:newY];
+            }
+            // move further in this column
+            currentY += yChange;
+        }
+        // move to the next column, start at the inital row
+        currentX += xChange;
+        currentY = initialY;
+    }
+}
+
+- (void)moveDice:(Dice *)die fromIndex:(NSInteger)oldX oldY:(NSInteger)oldY newX:(NSInteger)newX newY:(NSInteger)newY {
+    _gridArray[newX][newY] = _gridArray[oldX][oldY];
+    _gridArray[oldX][oldY] = _noTile;
+    CGPoint newPosition = [self positionForTile:newX row:newY];
+    CCActionMoveTo *moveTo = [CCActionMoveTo actionWithDuration:2.0f position:newPosition];
+    [die runAction:moveTo];
+}
+
+# pragma mark - Check indexes
+
+- (BOOL)indexValid:(NSInteger)x y:(NSInteger)y {
+//    BOOL indexValid = TRUE;
+//    indexValid &= x >= 0;
+//    indexValid &= y >= 0;
+//    if (indexValid) {
+//        indexValid &= x < GRID_ROWS;
+//        if (indexValid) {
+//            indexValid &= y < GRID_COLUMNS;
+//        }
+//    }
+    BOOL indexValid = YES;
+    if(x < 0 || y < 0 || x >= GRID_ROWS || y >= GRID_COLUMNS)
+    {
+        indexValid = NO;
+    }
+    return indexValid;
+}
+
+- (BOOL)indexValidAndUnoccupied:(NSInteger)x y:(NSInteger)y {
+    BOOL indexValid = [self indexValid:x y:y];
+    if (!indexValid) {
+        return FALSE;
+    }
+    BOOL unoccupied = [_gridArray[x][y] isEqual:_noTile];
+    return unoccupied;
 }
 
 @end
