8e5b3a2dea312b86c3566393de12fc86d08e8d3a
Nina Baculinao <nb2406@columbia.edu>
2014-07-28 15:33:08 -0400

    Fixed grid fill hole method including speed

--- ninabaculinao.spritebuilder/Source/Grid.m
+++ ninabaculinao.spritebuilder/Source/Grid.m
@@ -85,14 +85,18 @@ static const NSInteger GRID_COLUMNS = 6;
             if (![self canBottomMove]) {
                 [self scanForMatches];
                 [self spawnDice];
-                _timeSinceDrop = -0.2;
                 _dropInterval = 1.0;
+                _timeSinceDrop = 0.2;
             }
         } else if (stabilizing) {
             [self dieFillHoles];
             _timeSinceDrop = 0;
             _dropInterval = 0.1;
             stabilizing = [self checkGrid];
+            if (!stabilizing) {
+                _dropInterval = 1.0;
+                _timeSinceDrop = 0;
+            }
             // for each not stable die, move down
             // if cant move, set die.stable = true
             // once done, set stabilizing = false if all die are stable
