341b589fcc43536aef8036fa966be30a2309cb63
Nina Baculinao <nb2406@columbia.edu>
2014-08-14 22:30:11 -0400

    Pushing before setting up MGWU SDK

--- ninabaculinao.spritebuilder/Source/MainScene.m
+++ ninabaculinao.spritebuilder/Source/MainScene.m
@@ -12,45 +12,6 @@
 @implementation MainScene {
 }
 
-
-/* TODO: Calculate score system method - ended up doing it manually
- 
--(void)didLoadFromCCB {
-    
-    // Access score system dictionary
-    NSInteger face = 0;
-    NSInteger match;
-    NSInteger perfectMatch;
-    
-    NSString*sspath = [[NSBundle mainBundle] pathForResource:@"ScoreSystem" ofType:@"plist"];
-    NSDictionary *ssroot = [NSDictionary dictionaryWithContentsOfFile:sspath];
-    
-    NSArray *scoreSystem = [ssroot objectForKey: @"ScoreSystem"];
-    
-    NSDictionary *ssdict = scoreSystem[face-1];
-    match = [ssdict[@"score"] intValue];
-    perfectMatch = [ssdict[@"double"] intValue];
-    
-    // Access levels dictionary
-   
-    NSInteger level = 0;
-    NSInteger possibilities;
-    
-    NSString*lpath = [[NSBundle mainBundle] pathForResource:@"Levels" ofType:@"plist"];
-    NSDictionary *lroot = [NSDictionary dictionaryWithContentsOfFile:lpath];
-    
-    NSArray *levels = [lroot objectForKey: @"Levels"];
-    
-    NSDictionary *ldict = levels[level-1];
-    possibilities = [ldict[@"possibilities"] intValue];
-    
-    // Calculate target scores
-    // for each level, take all possibilities, get average, and multiply by desired moves
- 
-}
- 
- */
-
 - (void)play {
     CCLOG(@"play button pressed");
     [self performSelector:@selector(sandwichSpinAway)];
@@ -76,5 +37,43 @@
     [GameState sharedInstance].bestStreak = 0;
     [GameState sharedInstance].bestAllClear = 0;
 }
+
+/* TODO: Calculate score system method - ended up doing it manually
+ 
+ -(void)didLoadFromCCB {
+ 
+ // Access score system dictionary
+ NSInteger face = 0;
+ NSInteger match;
+ NSInteger perfectMatch;
+ 
+ NSString*sspath = [[NSBundle mainBundle] pathForResource:@"ScoreSystem" ofType:@"plist"];
+ NSDictionary *ssroot = [NSDictionary dictionaryWithContentsOfFile:sspath];
+ 
+ NSArray *scoreSystem = [ssroot objectForKey: @"ScoreSystem"];
+ 
+ NSDictionary *ssdict = scoreSystem[face-1];
+ match = [ssdict[@"score"] intValue];
+ perfectMatch = [ssdict[@"double"] intValue];
+ 
+ // Access levels dictionary
+ 
+ NSInteger level = 0;
+ NSInteger possibilities;
+ 
+ NSString*lpath = [[NSBundle mainBundle] pathForResource:@"Levels" ofType:@"plist"];
+ NSDictionary *lroot = [NSDictionary dictionaryWithContentsOfFile:lpath];
+ 
+ NSArray *levels = [lroot objectForKey: @"Levels"];
+ 
+ NSDictionary *ldict = levels[level-1];
+ possibilities = [ldict[@"possibilities"] intValue];
+ 
+ // Calculate target scores
+ // for each level, take all possibilities, get average, and multiply by desired moves
+ 
+ }
+ 
+ */
 
 @end
